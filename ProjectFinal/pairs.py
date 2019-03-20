dd20f6b2c4fbab5c15af569fb4e8e29d58643c00
David Grzan <dgrzan@ucdavis.edu>
2019-03-19 19:14:24 -0700

    works for a single pair for a simple budget

--- ProjectFinal/pairs.py
+++ ProjectFinal/pairs.py
@@ -10,11 +10,100 @@ from scipy.optimize import curve_fit
 import scipy.fftpack
 from matplotlib.backends.backend_pdf import PdfPages
 import random
+import trade
+import budgetcontrol
 
 
 if __name__ == "__main__":
 
+    #list of stocks that will be paired together, along with their names
+    pairslist = [("CVX","Chevron","XOM","ExxonMobil")]
+    portfolio = []
 
+    #looping through pairs of stocks
+    for stock1,stockname1,stock2,stockname2 in pairslist:
+        time1 = []
+        time2 = []
+        price1 = []
+        price2 = []
+        
+        #getting data from first stock
+        with open("/home/davidgrzan/Econophysics/ProjectFinal/StockData/"+stock1+".txt") as tsv:
+            for line in csv.reader(tsv,delimiter="\t"):
+                time1.append(float(line[0]))
+                price1.append(float(line[4]))
+            tsv.close()
+                
+        #getting data from second stock
+        with open("/home/davidgrzan/Econophysics/ProjectFinal/StockData/"+stock2+".txt") as tsv:
+            for line in csv.reader(tsv,delimiter="\t"):
+                time2.append(float(line[0]))
+                price2.append(float(line[4]))
+            tsv.close()
+
+        #truncating list to include only the past 10 years
+        time1 = time1[-2520:]
+        time2 = time2[-2520:]
+        price1 = price1[-2520:]
+        price2 = price2[-2520:]
+            
+        #calculating quantities
+        timecor252, cor252 = f.correlation(price1,price2,252,time1)
+        timecor504, cor504 = f.correlation(price1,price2,504,time1)
+        print(f.correlationall(price1,price2))
+        timeratio, priceratio = f.ratio(price1,price2,time1)
+        timesd3mo, movingsd3mo = f.movingsd(priceratio,63,timeratio)
+        timeavg3mo, movingavg3mo = f.movingavg(priceratio,63,timeratio)
+        timeavg5d, movingavg5d = f.movingavg(priceratio,5,timeratio)
+        low1, high1 = f.sdbounds(movingavg3mo,movingsd3mo,1)
+        zscoree = f.zscore(movingavg5d,movingavg3mo,movingsd3mo)
+        when, whenlist = f.whentotrade(zscoree,timeavg3mo,1.5)
+        #print(f.cointigration(price1[:],price1[:]))
+
+        #instantiates trade objects
+        trades = []
+        for tradetime in when:
+            location = np.sign(zscoree[f.indexoftime(timeavg3mo,tradetime)])
+            trades.append(trade.trade(price1,price2,time1,tradetime,location))
+            trades[len(trades)-1].set_inputamount(1000)
+
+        totalreturns = 0
+        for q in range(len(trades)):
+            starttime = trades[q].get_starttime()
+            startindex = f.indexoftime(timeavg3mo,starttime)
+            timezscore = timeavg3mo[startindex:]
+            for qq in range(len(zscoree[startindex:])-1):
+                if (np.sign(zscoree[startindex+qq+1])==1 and np.sign(zscoree[startindex+qq])==-1) or (np.sign(zscoree[startindex+qq+1])==-1 and np.sign(zscoree[startindex+qq])==1):
+                    trades[q].exittrade(timezscore[qq+1])
+                    break
+
+            totalreturns+=trades[q].get_totalreturns()
+        print("Total Profit: "+str(totalreturns))
+
+        portfolio.append(trades)
+        
+        #plotting
+        fig1 = plt.figure(1,figsize=(16,8))
+        ax1 = fig1.add_subplot(3,1,1)
+        ax2 = fig1.add_subplot(3,1,2)
+        ax3 = fig1.add_subplot(3,1,3)
+        ax1.plot(time1,price1,color="red")
+        ax1.plot(time2,price2,color="blue")
+        ax2.plot(timeratio,priceratio,color="green")
+        ax2.plot(timeavg3mo,movingavg3mo,color="orange")
+        ax2.plot(timeavg5d,movingavg5d,color="black")
+        ax2.plot(timeavg3mo,low1,color="red",linestyle="--")
+        ax2.plot(timeavg3mo,high1,color="red",linestyle="--")
+        ax3.plot(timecor252,cor252,color="green")
+        ax3.plot(timecor504,cor504,color="black")
+
+        fig2 = plt.figure(2,figsize=(16,8))
+        ax21 = fig2.add_subplot(1,1,1)
+        ax21.plot(timeavg3mo,zscoree,color="black")
+        ax21.plot(timeavg3mo,whenlist,color="red")
+        
+        plt.show()
+        plt.close()
 
 
     
--- ProjectFinal/pairsfunctions.py
+++ ProjectFinal/pairsfunctions.py
@@ -7,6 +7,8 @@ from scipy.optimize import curve_fit
 import scipy.fftpack
 from matplotlib.backends.backend_pdf import PdfPages
 import random
+import statsmodels.api as sm
+import pandas as pd
 
 #returns mean for set of data
 #mean(data list)
@@ -30,7 +32,7 @@ def sd(x):
 
 #returns correlation function number between two sets of data
 #correlation(data list 1, data list 2)
-def correlation(x1,x2):
+def correlationall(x1,x2):
     if len(x1)!=len(x2):
         print("ERROR: Invalid list sizes, lists must be of the same size")
         return float("nan")
@@ -49,15 +51,16 @@ def correlation(x1,x2):
 def correlation(x1, x2, timewindow, timelist):
     if len(x1)>len(x2):
         x1=x1[len(x1)-len(x2):]
+        if len(x1)==len(timelist):
+            timelist=timelist[len(x1)-len(x2):]
     if len(x1)<len(x2):
         x2=x2[len(x2)-len(x1):]
-    if len(x1)==len(timedinow):
-        
+        if len(x2)==len(timelist):
+            timelist=timelist[len(x2)-len(x1):]
     cor = []
     for i in range(len(x1)):
         if i+1>=timewindow:
-            for j in range(timewindow):
-                cor.append(correlation(x1[i-timewindow+1:i],x2[i-timewindow+1:i]))
+            cor.append(correlationall(x1[i-timewindow+1:i],x2[i-timewindow+1:i]))
     return timelist[timewindow-1:], cor
 
 #returns list of moving average of data
@@ -72,3 +75,80 @@ def movingavg(x,timewindow,timelist):
         if i+1>=timewindow:
             avg.append(mean(x[i+1-timewindow:i]))
     return timelist[timewindow-1:], avg
+
+#returns list of moving standard deviation
+#movingsd(data list, time window, time list)
+def movingsd(x,timewindow,timelist):
+    n = len(x)
+    if timewindow>n:
+        print("ERROR: time window is greater than length of list")
+        return float("nan")
+    movinsd = []
+    for i in range(n):
+        if i+1>=timewindow:
+            movinsd.append(sd(x[i+1-timewindow:i]))
+    return timelist[timewindow-1:], movinsd
+
+#returns two lists of standard deviations around the mean
+#sdbounds(moving average list, standard deviaion list, number of standard deviations from mean)
+def sdbounds(mvavg, sdlist, sds):
+    if len(mvavg)!=len(sdlist):
+        print("ERROR: list sizes are not the same")
+    high = []
+    low = []
+    for i in range(len(mvavg)):
+        high.append(mvavg[i]+sds*sdlist[i])
+        low.append(mvavg[i]-sds*sdlist[i])
+    return low, high
+
+#returns ratio of two stocks
+#ratio(data list 1, data list 2)
+def ratio(x1,x2,timelist):
+    rat = []
+    if len(x1)>len(x2):
+        x1=x1[len(x1)-len(x2):]
+        if len(x1)==len(timelist):
+            timelist=timelist[len(x1)-len(x2):]
+    if len(x1)<len(x2):
+        x2=x2[len(x2)-len(x1):]
+        if len(x2)==len(timelist):
+            timelist=timelist[len(x2)-len(x1):]
+    for i in range(len(x1)):
+        rat.append(x1[i]/x2[i])
+    return timelist, rat
+
+#returns cointigration number for two stocks
+#cointigration(x1, x2)
+def cointigration(x1, x2):
+    return sm.tsa.coint(x1,x2)
+
+#returns z = (ratio-mean)/sd
+#zscore(ratio,mean of ratio,sd of ratio)
+def zscore(ratio,meann,sdev):
+    z = []
+    ratio = ratio[-len(meann):]
+    for i in range(len(meann)):
+        z.append((ratio[i]-meann[i])/sdev[i])
+    return z
+
+#returns a list of index of times for when to trade, and a list to plot when trades happened
+#whentotrade(zscore list, time list, sd's threshold)
+def whentotrade(zscoree,timelist,sds):
+    when = []
+    whenlist = []
+    lastval = 0
+    for i in range(len(zscoree)):
+        whenlist.append(0)
+        if (lastval<sds and lastval>-sds) and (zscoree[i]>sds or zscoree[i]<-sds):
+            when.append(timelist[i])
+            whenlist[i]+=1
+        lastval = zscoree[i]
+    return when, whenlist
+
+#returns index of timelist where a given time is
+#indexoftime(timelist,time)
+def indexoftime(timelist,time):
+    for i in range(len(timelist)):
+        if timelist[i]==time:
+            return i
+    return -1
